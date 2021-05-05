fbcd835b1515c23556b9384357a739719056c6ab
Sagar M <a@a.com>
2021-05-05 14:53:32 +0530

    Removed logging Config changed to linux VM ones

--- src/main/java/com/sg/spark/controller/SparkController.java
+++ src/main/java/com/sg/spark/controller/SparkController.java
@@ -1,12 +1,10 @@
 package com.sg.spark.controller;
 
 import com.sg.spark.utils.SGKafkaConsumer;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Log4j2
 public class SparkController {
     @PostMapping("/kafka-outbound/message")
     public String processKafkaOutboundMessage() {
--- src/main/java/com/sg/spark/utils/SGSparkProducer.java
+++ src/main/java/com/sg/spark/utils/SGSparkProducer.java
@@ -1,20 +1,16 @@
 package com.sg.spark.utils;
 
-import lombok.extern.log4j.Log4j2;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
-import org.apache.spark.api.java.function.VoidFunction2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 @Component
-@Log4j2
 public class SGSparkProducer implements Serializable {
 
     @Value("${spark.appName}")
@@ -25,16 +21,17 @@ public class SGSparkProducer implements Serializable {
 
     public void mapToAsciis(String value) {
         SparkConf sc = new SparkConf().setAppName(sparkAppName).setMaster(sparkMaster);
-        log.debug("mapToAsciis method: " + value);
+        System.out.println("In mapToAsciis method: " + value);
         // new JavaSparkContext(sc).broadcast(value);
         JavaSparkContext javaSparkContext = new JavaSparkContext(sc);
         JavaRDD<String> splitValueList = javaSparkContext.parallelize(Arrays.asList(value.split(" ")));
-        log.debug("mapToAsciis method: " + "after Spark parallelize");
+        System.out.println("In mapToAsciis method: " + "after Spark parallelize");
         JavaRDD<String> mappedValues = splitValueList.flatMap(
                 (FlatMapFunction<String, String>) s -> Arrays.asList(s.toUpperCase()).iterator()
         );
+        System.out.println("In mapToAsciis method: " + "Values from spark");
         for (String str : mappedValues.collect()) {
-            log.debug(str);
+            System.out.println(str);
         }
         javaSparkContext.close();
 
